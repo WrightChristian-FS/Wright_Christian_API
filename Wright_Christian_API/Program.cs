8ea25cff26ca752af4e936108682b76ddf0dc155
WrightChristian-FS <cmwright@student.fullsail.edu>
2021-07-27 14:59:42 -0700

    Created API Application

---
+++ Wright_Christian_API/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wright_Christian_API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
