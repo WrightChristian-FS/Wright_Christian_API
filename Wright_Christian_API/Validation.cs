3b5261c76c0f542d916bfc3970b78256966f908b
WrightChristian-FS <cmwright@student.fullsail.edu>
2021-07-27 15:26:05 -0700

    Created validation class and functionality

--- Wright_Christian_API/Validation.cs
+++ Wright_Christian_API/Validation.cs
@@ -6,5 +6,45 @@ namespace Wright_Christian_API
         public Validation()
         {
         }
+
+        public static string StringValidation(string userInput)
+        {
+            while (string.IsNullOrWhiteSpace(userInput))
+            {
+                //State the problem
+                Console.WriteLine("\r\nSorry, this can not be blank or empty");
+
+                //Repeat the problem
+                Console.Write("\r\nPlease enter a valid entry: ");
+
+                //Capture the answer
+                UI.InputUI(); 
+                userInput = Console.ReadLine();
+                UI.StandardUI(); 
+            }
+
+            return userInput; 
+        }
+
+        public static int IntegerValidation(string userInput)
+        {
+            int convertedInput;
+
+            while (!(int.TryParse(userInput, out convertedInput)))
+            {
+                //State the problem
+                Console.WriteLine("\r\nSorry that is not a valid positive number!");
+
+                //Repeat the question
+                Console.Write("\r\nPlease enter a number: ");
+
+                //Capture the answer
+                UI.InputUI();
+                userInput = Console.ReadLine();
+                UI.StandardUI(); 
+            }
+
+            return convertedInput; 
+        }
     }
 }
